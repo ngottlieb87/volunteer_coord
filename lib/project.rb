f34a8451d14144e1953ffd1c2ec30fa4e065e90a
ngottlieb87 <nogpdx@gmail.com>
2017-09-22 11:41:55 -0700

    add working volunteer method to project

--- lib/project.rb
+++ lib/project.rb
@@ -31,6 +31,16 @@ class Project
     return nil
   end
 
+  def volunteers
+    project_volunteers= []
+    volunteers = DB.exec("SELECT * FROM volunteers WHERE project_id = #{self.id};")
+    volunteers.each do |volunteer|
+      name = volunteer["name"]
+      project_id = volunteer["project_id"].to_i
+      project_volunteers.push(Volunteer.new({:name => name, :project_id => project_id}))
+    end
+    project_volunteers
+  end
 
   def save
     @id= DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;").first.fetch("id").to_i()
