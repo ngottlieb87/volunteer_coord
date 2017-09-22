aea60087ca6a1ccd6445f60276e15b739a9a2711
ngottlieb87 <nogpdx@gmail.com>
2017-09-22 11:24:12 -0700

    add working spec for empty .all

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -10,4 +10,15 @@ class Volunteer
     self.name.==(another_volunteer.name).&(self.project_id.==(another_volunteer.project_id))
   end
 
+  def self.all
+    volunteers = DB.exec("SELECT * FROM volunteers;")
+    all_volunteers = []
+    volunteers.each do |volunteer|
+      name = volunteer["name"]
+      project_id = volunteer["project_id"].to_i
+      all_volunteers.push(Project.new({:name => name, :project_id=> project_id}))
+    end
+    all_volunteers
+  end
+
 end
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -14,7 +14,7 @@ describe Volunteer do
       expect(test_volunteer.project_id).to eq 1
     end
   end
-  
+
   describe '#==' do
     it 'checks for equality based on the name of a volunteer' do
       volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
@@ -22,21 +22,22 @@ describe Volunteer do
       expect(volunteer1 == volunteer2).to eq true
     end
   end
-  #
-  # context '.all' do
-  #   it 'is empty to start' do
-  #     expect(Volunteer.all).to eq []
-  #   end
-  #
-  #   it 'returns all volunteers' do
-  #     volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-  #     volunteer1.save
-  #     volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
-  #     volunteer2.save
-  #     expect(Volunteer.all).to eq [volunteer1, volunteer2]
-  #   end
-  # end
-  #
+
+  context '.all' do
+    it 'is empty to start' do
+      expect(Volunteer.all).to eq []
+    end
+  end
+
+    it 'returns all volunteers' do
+      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      volunteer1.save
+      volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
+      volunteer2.save
+      expect(Volunteer.all).to eq [volunteer1, volunteer2]
+    end
+  end
+
   # describe '#save' do
   #   it 'adds a volunteer to the database' do
   #     volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
